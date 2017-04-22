1ee9be42124b0586fdc364c2ef0c73a65c61748a
Robert Brock <robert.wayne.brock@gmail.com>
2017-04-22 19:23:30 -0400

    layout revisions

--- comic.php
+++ comic.php
@@ -3,54 +3,61 @@
 <head>
   <meta charset="utf-8">
   <title>Brock n Broll</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"> -->
+  <link rel="stylesheet" href="css/foundation.css">
   <link rel="stylesheet" href="css/app.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <!--<link rel="stylesheet" href="css/site_main.css">-->
+  <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
   <script src="js/vue.js"></script>
 
 </head>
 <body>
-  <?php
-    echo $_GET['page'] ?? 'empty';
-  ?>
-  <div class="container">
-    <div class="row" id="mainBody">
-      <div class="col-md-1 menu">
-        <div>
-          <ul class="nav navbar-nav">
-            <li><a href="#">Comic</a></li>
-            <!--<li><a href="#">Words</a></li>-->
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">About</a></li>
+<div class="row">
+  <div class="large-2 columns" id="siteNav">
+    <ul class="menu vertical">
+      <li><a href="comic.php">Comic</a></li>
+      <!--<li><a href="#">Words</a></li>-->
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">About</a></li>
+    </ul>
+    <div>
+      <img src="img/brock-n-broll-banner.png" alt="Brock n Broll" />
+    </div>
+  </div>
+  <div class="large-10 columns full-height" id="mainContent">
+    <div class="view-frame">
+      <div id="comic">
+        <h3 style="text-align:center;">{{comic.title}}</h3>
+        <div class="comicNav">
+          <ul class="menu expanded">
+            <li><a v-bind:href="comic.url+comic.pagingFirst">First</a></li> 
+            <li><a v-bind:href="comic.url+comic.pagingPrevious">Previous</a></li>  
+            <li>{{comic.id}}</li>  
+            <li><a v-bind:href="comic.url+comic.pagingNext">Next</a></li> 
+            <li><a v-bind:href="comic.url+comic.pagingLast">Last</a></li> 
           </ul>
         </div>
-      </div>
-      <div class="col-md-11" id="mainContent">
-        <div class="view-frame">
-          <div id="comic">
-            <h2>{{comic.title}}</h2>
-            <div class="paging">
-              <a v-bind:href="comic.url+comic.pagingFirst">First</a> | 
-              <a v-bind:href="comic.url+comic.pagingPrevious">Previous</a> |  
-              <a v-bind:href="comic.url+comic.pagingNext">Next</a> | 
-              <a v-bind:href="comic.url+comic.pagingLast">Last</a>
+        <div><img v-bind:alt="comic.altAttr" v-bind:src="comic.fileName" v-bind:title="comic.titleAttr" ></div>
+        <div class="comicNav">
+          <ul class="menu expanded">
+            <li><a v-bind:href="comic.url+comic.pagingFirst">First</a></li> 
+            <li><a v-bind:href="comic.url+comic.pagingPrevious">Previous</a></li>
+            <li>{{comic.id}}</li>  
+            <li><a v-bind:href="comic.url+comic.pagingNext">Next</a></li> 
+            <li><a v-bind:href="comic.url+comic.pagingLast">Last</a></li> 
+          </ul>
+        </div>
+        <div class="row">
+          <div class="large-12 columns">
+            <div class="comicDescription">
+              <div v-html="comic.description"></div>
+              <div>&mdash;{{comic.user}}</div>
             </div>
-            <div><img v-bind:alt="comic.altAttr" v-bind:src="comic.fileName" v-bind:title="comic.titleAttr" ></div>
-            <div class="paging">
-              <a v-bind:href="comic.url+comic.pagingFirst">First</a> | 
-              <a v-bind:href="comic.url+comic.pagingPrevious">Previous</a> |  
-              <a v-bind:href="comic.url+comic.pagingNext">Next</a> | 
-              <a v-bind:href="comic.url+comic.pagingLast">Last</a>
-            </div>
-            <div>{{comic.user}}</div>
-            <div v-html="comic.description"></div>
           </div>
-		    </div>
+        </div>
       </div>
     </div>
   </div>
+</div>
   <script>
   $(function(){
     
