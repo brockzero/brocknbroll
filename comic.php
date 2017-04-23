efd37de6afd7a603a2f33334cf672a200b049ee6
Robert Brock <robert.wayne.brock@gmail.com>
2017-04-22 20:00:04 -0400

    updated responsive minimums

--- comic.php
+++ comic.php
@@ -8,10 +8,9 @@
   <!--<link rel="stylesheet" href="css/site_main.css">-->
   <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
   <script src="js/vue.js"></script>
-
 </head>
 <body>
-<div class="row">
+<div class="row main">
   <div class="large-2 columns" id="siteNav">
     <ul class="menu vertical">
       <li><a href="comic.php">Comic</a></li>
@@ -21,6 +20,7 @@
     </ul>
     <div>
       <img src="img/brock-n-broll-banner.png" alt="Brock n Broll" />
+      <h1>Brock 'N Broll</h1>
     </div>
   </div>
   <div class="large-10 columns full-height" id="mainContent">
